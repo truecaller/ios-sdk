e8c0c5de91b80be65710deda1c890a6f9ca19d26
guveniscan <guven.iscan@truecaller.com>
2017-03-09 10:36:39 +0100

    Move assets and strings to resource bundle Refine version compatibility checking Add version header Update version to 0.2.0

--- TrueSDK/Core/TCUtils.h
+++ TrueSDK/Core/TCUtils.h
@@ -32,6 +32,12 @@
 + (void)openUrl:(NSURL*)url completionHandler:(void (^)(BOOL success))completion;
 
 /*!
+ * @brief Get the resource bundle for TrueSDK
+ * @return bundle
+ */
++ (NSBundle *)resourcesBundle;
+
+/*!
  * @brief Get the API version of the TrueSDK
  * @return version
  */
@@ -60,4 +66,5 @@
  * @return version
  */
 + (NSString *)getMinSupportedSDKVersion;
+
 @end
--- TrueSDK/Core/TCUtils.m
+++ TrueSDK/Core/TCUtils.m
@@ -9,8 +9,16 @@
 #import "TCUtils.h"
 #import <UIKit/UIKit.h>
 #import "TCTrueSDKLogger.h"
+#import "TCVersion.h"
 
 @implementation TCUtils
+
++ (void) initialize
+{
+    //Version plist fetching for pod installations
+    NSBundle *bundle = [self resourcesBundle];
+    NSURL *dictUrl = [bundle URLForResource:@"TrueSDK" withExtension:@"plist"];
+}
 
 + (BOOL)isOperatingSystemSupported
 {
@@ -47,19 +55,28 @@
     }
 }
 
++ (NSBundle *)resourcesBundle
+{
+    NSBundle *frameworkBundle = [NSBundle bundleForClass:[self class]];
+    NSURL *bundleUrl = [frameworkBundle.resourceURL URLByAppendingPathComponent:@"TrueSDK.bundle"];
+    NSBundle *bundle = [NSBundle bundleWithURL:bundleUrl];
+    
+    return bundle ?: frameworkBundle;
+}
+
 + (NSString *)getAPIVersion
 {
-    return [[NSBundle bundleForClass:[self class]] objectForInfoDictionaryKey:@"TCApiVersion"];
+    return TrueSDKApiVersion;
 }
 
 + (NSString *)getSDKVersion
 {
-    return [[NSBundle bundleForClass:[self class]] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+    return TrueSDKVersion;
 }
 
 + (NSString *)getMinSupportedAPIVersion
 {
-    return [[NSBundle bundleForClass:[self class]] objectForInfoDictionaryKey:@"TCMinSupportedApiVersion"];
+    return TrueSDKMinSupportedApiVersion;
 }
 
 + (NSString *)getMaxSupportedAPIVersion
@@ -69,7 +86,7 @@
 
 + (NSString *)getMinSupportedSDKVersion
 {
-    return [[NSBundle bundleForClass:[self class]] objectForInfoDictionaryKey:@"TCMinSupportedSdkVersion"];
+    return TrueSDKMinSupportedVersion;
 }
 
 @end
